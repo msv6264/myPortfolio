72752efdf36716c5a3f161e210cb50a48061786f
Sri Vaishnavi <srivaishnavi0602@gmail.com>
2026-04-10 15:40:50 +0530

    refactoring

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -4,9 +4,49 @@ import swoc from "../assets/SWOC_img.png";
 import acwoc from "../assets/ACWOC_img.png";
 import home from "../assets/home.svg";
 import { useNavigate } from "react-router-dom";
+import { duration } from '@mui/material';
 
 export default function Experience() {
   const navigate = useNavigate();
+
+  const exp = [
+    {
+      expName: "Open Source contributor",
+      expIcon: gssoc,
+      duration: "Oct 2024 - Nov 2024",
+      desc:[
+        {pt: "Collaborated with project managers and like fellow contributors, made some meaningful contributions."},
+        {pt: "Learned and applied real-world collaboration through Git and GitHub."},
+        {pt: "Solved many issues including Ul enhancements, bug fixes, and feature development."},
+        {pt: "Improved dark mode contrast to enhance accessibility and UX."}
+      ],
+      skills: "HTML, CSS, JS, React, Git, GitHub",
+    },
+    {
+      expName: "Open Source contributor",
+      expIcon: swoc,
+      duration: "Jan 2025 - Mar 2025",
+      desc:[
+        {pt: "Contributed to 6 amazing projects, created animations, preloaders, Fixed bugs, enhanced UI"},
+        {pt: "I am familiar with version control systems like git which makes the work flow easier"},
+        {pt: "Achieved 23rd rank amoung 22k people."},
+        {pt: "Received 810 points which are awarded according to the level of contributions starting from 10 points."},
+      ],
+      skills: "HTML, CSS, JS, React, Git, GitHub, Tailwind css, Bootstrap",
+    },
+    {
+      expName: "Open Source contributor",
+      expIcon: acwoc,
+      duration: "Feb 2025 - Mar 2025",
+      desc:[
+        {pt: "Contributed to some amazing projects, designed logo's, created animations, preloaders, Fixed bugs, enhanced UI"},
+        {pt: "I collaborate with project managers and mentors to successfully implement anything."},
+        {pt: "Ranked 14th and received digital badge."},
+        {pt: "Also rewarded top contributor certificate."},
+      ],
+      skills: "HTML, CSS, JS, React, Git, GitHub, Tailwind css, Bootstrap",
+    },
+  ]
 
   return (
     <div className="bg-[#050619] min-h-screen w-full m-auto p-3">
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -10,76 +10,122 @@ import gitHub from "../assets/github-icon.svg";
 import tailwind from "../assets/tailwind-icon.svg";
 import { useNavigate } from "react-router-dom";
 import home from "../assets/home.svg";
+import fstApi from "../assets/fstApi.png";
+import flsk from "../assets/flsk.png";
+import nodeIcon from "../assets/nodeIcon.png";
+import xprss from "../assets/xprss.png";
 
-function Skills(){
-    const navigate = useNavigate();
+function Skills() {
+  const navigate = useNavigate();
+  const skillObj = [
+    {
+      skillName: "HTML",
+      iconWidth: "120px",
+      iconSrc: html,
+    },
+    {
+      skillName: "CSS",
+      iconWidth: "120px",
+      iconSrc: css,
+    },
+    {
+      skillName: "JS",
+      iconWidth: "80px",
+      iconSrc: js,
+    },
+    {
+      skillName: "React",
+      iconWidth: "100px",
+      iconSrc: react,
+    },
+    {
+      skillName: "Flask",
+      iconWidth: "100px",
+      iconSrc: flsk,
+    },
+    {
+      skillName: "NodeJs",
+      iconWidth: "100px",
+      iconSrc: nodeIcon,
+    },
+    {
+      skillName: "ExpressJS",
+      iconWidth: "100px",
+      iconSrc: xprss,
+    },
+    {
+      skillName: "FastAPI",
+      iconWidth: "90px",
+      iconSrc: fstApi,
+    },
+    {
+      skillName: "Bootstrap",
+      iconWidth: "90px",
+      iconSrc: bootstrap,
+    },
+    {
+      skillName: "Tailwind CSS",
+      iconWidth: "100px",
+      iconSrc: tailwind,
+    },
+    {
+      skillName: "C language",
+      iconWidth: "90px",
+      iconSrc: c,
+    },
+    {
+      skillName: "Python",
+      iconWidth: "90px",
+      iconSrc: python,
+    },
+    {
+      skillName: "Git",
+      iconWidth: "100px",
+      iconSrc: git,
+    },
+    {
+      skillName: "GitHub",
+      iconWidth: "100px",
+      iconSrc: gitHub,
+    },
+  ];
 
-    return(
-        <div className="bg-[#050619] min-h-screen w-full relative overflow-hidden">
-            <div className="homeIcon w-12 h-12 rounded-full border border-[aqua] flex items-center cursor-pointer justify-center absolute m-16" onClick={() => navigate("/")} >
-                <img src={home} onClick={() => navigate("/")} className="w-8 h-8 cursor-pointer" />
-            </div>
-      
-            <div className="flex flex-col  items-center mt-10 mb-14 gap-16">
-                <div className="rounded-lg flex flex-col text-5xl winky-rough-header text-[#f7fb08]">
-                    Tech Stack
-                </div>
-                
-                <div className="flex flex-wrap gap-20 winky-rough-header text-[aqua] text-xl pl-[8%] w-[60%]">
-                <div className="w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 gap-2">
-                    <img className="w-20" src={c} alt="C language logo" />
-                    <div>C language</div>
-                </div>
-                    
-                    <div className="shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center gap-2">
-                        <img className="w-20" src={python} /> 
-                        <div>python</div>
-                    </div>
+  return (
+    <div className="bg-[#050619] min-h-screen w-full relative overflow-hidden">
+      <div
+        className="homeIcon w-12 h-12 rounded-full border border-[aqua] flex items-center cursor-pointer justify-center absolute m-16"
+        onClick={() => navigate("/")}
+      >
+        <img
+          src={home}
+          onClick={() => navigate("/")}
+          className="w-8 h-8 cursor-pointer"
+        />
+      </div>
 
-                    <div className="shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center gap-2">
-                        <img className="w-21" src={html} /> 
-                        <div>HTML</div>
-                    </div>
-
-                    <div className="shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center gap-2">
-                        <img className="w-21" src={css} /> 
-                        <div>CSS</div>
-                    </div>
-
-                    <div className="shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center gap-2">
-                        <img className="w-20" src={js} /> 
-                        <div>JS</div>
-                    </div>
-
-                    <div className="shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center gap-2">
-                        <img className="w-[100px]" src={bootstrap} /> 
-                        <div>Bootstrap</div>
-                    </div>
-
-                    <div className="shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center gap-2">
-                        <img className="w-[100px]" src={tailwind} /> 
-                        <div>Tailwind CSS</div>
-                    </div>
-
-                    <div className="shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center gap-2">
-                        <img className="w-[105px]" src={react} /> 
-                        <div>React</div>
-                    </div>
-
-                    <div className="shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center gap-2">
-                        <img className="w-[105px]" src={git} /> 
-                        <div>Git</div>
-                    </div>
-
-                    <div className="shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center gap-2">
-                        <img className="w-[105px]" src={gitHub} /> 
-                        <div>GitHub</div>
-                    </div>
-
-                </div>
-            </div>
+      <div className="flex flex-col  items-center mt-10 mb-14 gap-16">
+        <div className="rounded-lg flex flex-col text-5xl winky-rough-header text-[#f7fb08]">
+          Tech Stack
         </div>
-    );
+
+        <div className="flex flex-wrap gap-20 winky-rough-header text-[aqua] text-xl pl-[8%] w-[60%]">
+          {skillObj.map((skill, idx) => (
+            <div
+              key={idx}
+              className="w-[180px] h-[180px] rounded-lg flex flex-col justify-center items-center shadow-lg shadow-[#7db4b4] hover:scale-110 transition-transform ease-in-out duration-300 gap-2"
+            >
+              <img
+                style={{ width: skill.iconWidth }}
+                src={skill.iconSrc}
+                alt={skill.skillName}
+              />
+              <div>{skill.skillName}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
